2edff21961bc774ce39e6f650ea0e5f86da77d47
Athul Rajagopal <49791750+rajagoat@users.noreply.github.com>
2021-11-06 13:33:24 -0600

    temp

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import './App.css';
 import Dashboard from './pages/Dashboard';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path='/' />
-        </Routes>
-        <Dashboard />
-      </div>
-    </Router>
+    <div className="App">
+      <Dashboard />
+    </div>
   );
 }
 export default App;
--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,21 @@
 import React from "react";
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Page2 from "./pages/Page-2";
 import Portfolio from "./pages/Portfolio";
 import Dashboard from "./pages/Dashboard";
 import NotFound from "./pages/NotFound";
 
-/*function Routes() {
+function Routes() {
     return (
         <BrowserRouter>
-            <Switch>
+            <Routes>
                 <Route path="/" exact component={Dashboard}/>
                 <Route path="/portfolio" component={Portfolio}/>
                 <Route path="/page-2" component={Page2}/>
                 <Route component={NotFound}/>
-            </Switch>
+            </Routes>
         </BrowserRouter>
     )
 }
 
-export default Routes;*/
+export default Routes;
